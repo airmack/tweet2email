ae2a13f5f83489faf092e235e3d900a3e327ad7c
airmack <airmack@gentoofighter.(none)>
2010-08-05 18:12:41 +0200

    Eigentlich fertig, es fehlen nur ein test

---
+++ tweet2mail.mm
@@ -0,0 +1,49 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1281003450260" ID="Freemind_Link_960063709" MODIFIED="1281004305877" TEXT="tweet2mail">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1281003464512" ID="_" MODIFIED="1281004305844" POSITION="right" TEXT="json">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1281003653688" ID="Freemind_Link_917382255" MODIFIED="1281004305846" TEXT="http://twitter.com/account/verify_credentials.json">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1281004053765" ID="Freemind_Link_815193967" MODIFIED="1281004305850" TEXT="http://twitter.com/statuses/friends.json">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1281003488201" ID="Freemind_Link_1786783685" MODIFIED="1281004305852" POSITION="left" TEXT="smtp">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1281003492821" ID="Freemind_Link_689421140" MODIFIED="1281004305855" POSITION="right" TEXT="mechanize">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1281003509390" ID="Freemind_Link_1388673322" MODIFIED="1281004305858" TEXT="login">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1281003513104" ID="Freemind_Link_365869922" MODIFIED="1281004305862" TEXT="user">
+<arrowlink DESTINATION="Freemind_Link_1485483388" ENDARROW="Default" ENDINCLINATION="233;26;" ID="Freemind_Arrow_Link_416383540" STARTARROW="None" STARTINCLINATION="303;0;"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1281003516928" ID="Freemind_Link_1005950281" MODIFIED="1281004305864" TEXT="password">
+<arrowlink DESTINATION="Freemind_Link_1485483388" ENDARROW="Default" ENDINCLINATION="131;27;" ID="Freemind_Arrow_Link_1029801664" STARTARROW="None" STARTINCLINATION="331;30;"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1281003521755" ID="Freemind_Link_776024947" MODIFIED="1281004305867" TEXT="get latest tweets">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1281003529462" ID="Freemind_Link_1570344176" MODIFIED="1281004305870" TEXT="message ID">
+<arrowlink DESTINATION="Freemind_Link_1485483388" ENDARROW="Default" ENDINCLINATION="59;2;" ID="Freemind_Arrow_Link_1925261125" STARTARROW="None" STARTINCLINATION="121;35;"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1281003537930" ID="Freemind_Link_429598095" MODIFIED="1281004305872" TEXT="message">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1281003547019" ID="Freemind_Link_332089208" MODIFIED="1281004305874" TEXT="prevent dupplicates">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1281003554241" ID="Freemind_Link_1485483388" MODIFIED="1281004305876" TEXT="sqliteDB">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
